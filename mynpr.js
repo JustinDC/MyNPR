e67043aad0c7df162fecce3743c02b544795fa31
DavidKim2015 <DavidKim2015@u.northwestern.edu>
2014-11-06 15:16:08 -0600

    stuff

--- mynpr.js
+++ mynpr.js
@@ -1,23 +1,70 @@
 
 var main = function() {
-    $.get('http://api.npr.org/query?id=1149&numResults=20&apiKey=MDE2OTQ2ODMxMDE0MTI1NDIzODY0YjNiMg001', getnpr);
+    $.get('http://api.npr.org/query?id=1149&numResults=2&apiKey=MDE2OTQ2ODMxMDE0MTI1NDIzODY0YjNiMg001', getnpr);
 }
-
-var queryRunning = false;
-var mp3Links = [];
-var numStories = 0;
 
 var getnpr = function(nprml) {
 	var nprlist = $(nprml).find('list')[0];
 	var stories = $(nprlist).find('story');
-	queryRunning = true;
-	mp3Links = [];
 	numStories = stories.length;
+	var storyObjects = [];
+	/*Story object is an Object containing the following fields for each story
+	title
+	duration
+	primaryTopic
+	topics (as an array of strings)
+	link
+	ids of related links (as an array of numbers)
+	m3u
+	mp3
+
+	in the future maybe:
+	teaser
+	image thumbnail
+	*/
 	for (var i = 0; i < stories.length; i++) {
 		var story = $(stories)[i];
 		var mp3 = $(story).find('mp3');
-		if (mp3.length == 1) {
-			$.get($(mp3).text(), processm3u);
+		if (mp3.length == 1) { //viable story, has an mp3 file
+			var storyObject = {};
+			storyObject.title = $($(story).find('title')[0]).text();
+			storyObject.duration = $($(story).find('duration')[0]).text();
+			var topics = [];
+			var primaryTopic;
+			var tags = [];
+			var program;
+			console
+			for (var j = 0; j < $(story).find('parent').length; j++) {
+				var parentType = $($(story).find('parent')[j]).attr('type');
+				var parentId = $($(story).find('parent')[j]).attr('id');
+				if (parentType == 'topic') {
+					topics.push(parentId);
+				}
+				if (parentType == 'primaryTopic')
+					primaryTopic = parentId;
+				if (parentType == 'tag')
+					tags.push(parentId);
+				if (parentType == 'program')
+					program = parentId;
+			}
+			storyObject.topics = topics;
+			storyObject.primaryTopic = primaryTopic;
+			storyObject.tags = tags;
+			storyObject.program = program;
+
+			storyObject.link = $($(story).find('link')[0]).text();
+
+			var relatedLinks = [];
+			for (var j = 0; j < $(story).find('relatedLink'); j++) {
+				var relatedLink = $($(story).find('relatedLink')[j]).attr('id');
+				relatedLinks.push(relatedLink);
+			}
+			storyObject.relatedLinks = relatedLinks;
+			console.log(story);
+			console.log(storyObject);
+			//console.log($($(story).find('parent')[0]).attr('type'));
+			//console.log($($(story).find('parent')[0]).attr('id'));
+			//console.log(storyObject);
 		}
 	}
 }
